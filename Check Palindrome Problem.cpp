6a24cc989c4c6ec7772be69c2a3cbcb0bd6af0f7
Jashvant Sharma <88959637+Jashvants@users.noreply.github.com>
2022-06-30 13:06:27 +0530

    Check string pelindrome coding ninja

---
+++ Check Palindrome Problem.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+/* Time complexity: O(n)
+   Space complexity: O(1)
+   */
+
+int length(char str[]){
+	int count=0;
+	for(int i=0;str[i]!='\0';i++){
+		count++;
+	}
+	return count;
+}
+
+
+
+bool checkPalindrome(char str[]){
+	for(int i=0;i<=length(str)/2;i++){
+		if(str[i]!=str[length(str) - 1 - i]){
+			return false;
+		}
+	}
+	return true;
+	
+}
+
+
+
+int main() {
+    int size = 1e6;
+    char str[size];
+    cin >> str;
+    cout << (checkPalindrome(str) ? "true" : "false");
+}
