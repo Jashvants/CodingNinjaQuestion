7b7c07930c834c5f38dd6b71032aee48015dbfe5
Jashvant Sharma <88959637+Jashvants@users.noreply.github.com>
2022-07-06 11:20:01 +0530

    Pointer basic

---
+++ PointerQues.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+//	int a[]={1,2,3,4};
+//	char b[]="abc";
+//	cout<<a<<endl;
+//	cout<<b<<endl;
+//	
+//	char* c=&b[0];
+//	cout<<c<<endl;
+//	
+//	char c1='a';
+//	char* pc=&c1;
+//	
+//	cout<<c1<<endl;
+//	cout<<pc<<endl;
+
+//    char ch='a';
+//    char * ptr=&ch;
+//    ch++;
+//    cout<<*ptr<<endl;
+//    
+//      char b[]="xyz";
+//      char *c =&b[0];
+//      cout<<c<<endl;
+//      char b[]= "xyz";
+//      char *c =&b[0];
+//      c++;
+//      cout<<c<<endl;
+
+     char s[]="hello";
+     char *p=s;
+     cout<<s[0]<<" "<<p[0];
+
+
+	
+}
